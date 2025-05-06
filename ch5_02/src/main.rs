cc2797ef265b2549aa44bbab06077bd7bc0fc9d6
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-05-06 15:37:13 +0530

    chore: add struct program

---
+++ ch5_02/src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn calculate_area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    
+    println!("The area of the rectangle with dimenstions {rect1:?} is {} square pixels.", calculate_area(&rect1));
+}
